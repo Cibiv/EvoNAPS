f7a450556ca8ea0404d53f70dbfd73a7fa7be96b
JCYrepresents <jerryokoli@gmail.com>
2023-09-14 15:01:43 +0200

    Isset test

--- EvoNAPS_webinterface/EvoNAPS_Newest_V/variables_mp.php
+++ EvoNAPS_webinterface/EvoNAPS_Newest_V/variables_mp.php
@@ -2,67 +2,77 @@
 
 session_start();
 //error_reporting(0);
-
+		if(isset($_POST['tree_features'])){
 		$DNA_Prot = $_POST['datatype'];
 		$_SESSION['datatype']= $DNA_Prot; 
-		
-		
+		}
+		if(isset($_POST['tree_features'])){
 		$Matrices_D = $_POST['DNA_model'];
 		$_SESSION['DNA_model']= $Matrices_D; 
-		
-		
+		}
+		if(isset($_POST['tree_features'])){
 		$Matrices_P = $_POST['Protein_model'];
 		$_SESSION['Protein_model']= $Matrices_P; 
-		
+		}
 		
 		//$modelp = $Matrices_P." ".$RHAS;
 		//$modeld = $Matrices_D." ".$RHAS;
 		
+		if(isset($_POST['tree_features'])){
 		$OPT_uOPT = $_POST['query_type'];
 		$_SESSION['query_type']= $OPT_uOPT; 
-		
+		}
+		if(isset($_POST['tree_features'])){
 		$AIC = $_POST['AIC'];
 		$_SESSION['AIC']= $AIC; 
-		
+		}
+		if(isset($_POST['tree_features'])){
 		$AICC = $_POST['AICC'];
 		$_SESSION['AICC']= $AICC; 
-		
+		}
+		if(isset($_POST['tree_features'])){
 		$BIC = $_POST['BIC'];
 		$_SESSION['BIC']= $BIC; 
-		
+		}
+		if(isset($_POST['tree_features'])){		
 		$Nr_Seq = $_POST['number_of_sequences'];
 		$_SESSION['number_of_sequences']= $Nr_Seq; 
-		
+		}
+		if(isset($_POST['tree_features'])){
 		$Max_Nr_Seq = $_POST['max_number_of_sequences'];
 		$_SESSION['max_number_of_sequences']= $Max_Nr_Seq; 
-		
-		
+		}
+		if(isset($_POST['tree_features'])){
 		$Nr_sites = $_POST['number_of_sites'];
 		$_SESSION['number_of_sites']= $Nr_sites; 
-		
+		}
+
+		if(isset($_POST['tree_features'])){
 		$Max_Nr_sites = $_POST['max_number_of_sites'];
 		$_SESSION['max_number_of_sites']= $Max_Nr_sites; 
-		
-		
+		}
+		if(isset($_POST['tree_features'])){
 		$mean_dis = $_POST['mean_distance'];
 		$_SESSION['mean_distance']= $mean_dis; 
-		
+		}
+		if(isset($_POST['tree_features'])){
 		$Max_mean_dis = $_POST['max_mean_distance'];
 		$_SESSION['max_mean_distance']= $Max_mean_dis; 
-		
-		
+		}
+		if(isset($_POST['tree_features'])){
 		$tree_len = $_POST['tree_length'];
-		$_SESSION['tree_length']= $tree_len; 
-		
+		$_SESSION['tree_length']= $tree_len; #
+		}
+		if(isset($_POST['tree_features'])){
 		//$tree_top = $_POST['tree_top'];
 		$tree_dia = $_POST['tree_diameter'];
 		$_SESSION['tree_diameter']= $tree_dia; 
-		
-		
+		}
+		if(isset($_POST['tree_features'])){
 		$Alignment_Specs_Check = $_POST['alignment_features'];
 		$_SESSION['alignment_features']= $Alignment_Specs_Check; 
 		
-		
+		}
 		if(isset($_POST['tree_features'])){
 			
 		$Trees_Specs_Check = $_POST['tree_features'];
@@ -71,20 +81,15 @@ session_start();
 		}
 		
 		
-		
-		$Hits = $_POST['Hits_anzeigen'];
-		$_SESSION['Hits_anzeigen']= $Hits; 
-		
-		
 
 		
 
 		
-		
+		if(isset($_POST['tree_features'])){
 		$NewWick = $_POST['Newick'];
 		$_SESSION['Newick']= $NewWick; 
 		//Variables for the Hit
-		
+		}
 		 
 		
 		
