1cc7cd298fb5f5ebc3f8c9e4abd62c9779b95a37
JCYrepresents <jerryokoli@gmail.com>
2023-10-31 15:57:12 +0100

    minor fixes

--- EvoNAPS_webinterface/EvoNAPS_Newest_V/results_trees.php
+++ EvoNAPS_webinterface/EvoNAPS_Newest_V/results_trees.php
@@ -4,8 +4,9 @@
   <head>
     <meta charset="utf-8">
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	  <!--Bootstrap include -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-	<!-- Bootstrap Font Icon CSS -->
+	<!-- Bootstrap Font Icon CSS include -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 	<div class = "title id" = "title" />
     <title>EvoNAPS</title>
